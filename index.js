31fcc0f978686f0c7ac5afa48d09be55e1ca0d68
Arnob Biswas <arnobbiswas@Arnobs-Mac-mini.local>
2025-02-20 18:20:34 +0600

    refactored the code of server and worker

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const http=require('http');
 const {handleReqRes}=require('./helpers/handleReqRes');
 const environment=require('./helpers/environments');
 const data=require('./lib/data');
+const server=require('./lib/server');
+const worker=require('./lib/worker');
 
 const app={};
 
-data.delete('test','newFile',(err)=>{
-    console.log(`error was`,err);
-})
+app.init=()=>{
+    server.init();
+    worker.init();
+};
 
-app.handleReqRes=handleReqRes;
+app.init();
 
-app.createServer=()=>{
-    const server=http.createServer(app.handleReqRes);
-    server.listen(environment.port,()=>{
-        console.log(`listening to port ${environment.port}`);
-    })
-}
-
-app.createServer();
+module.exports=app;
